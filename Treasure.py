d3550652902b81991fd2d3d194273d90d89cce89
Yahyaa Shaikh <90694064+Jackei10@users.noreply.github.com>
2024-01-05 00:09:55 +0530

    Update Treasure.py

--- Treasure.py
+++ Treasure.py
@@ -19,8 +19,25 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____/__
 *******************************************************************************''')
 print("Welcome to the Game of GUTS")
-choice1=input('You see a board saying "Danger ahead" would you still go ahead? yes or no').lower()
+choice1=input('You are on a road somewhere in a forest & you see a board saying "Danger ahead" would you still go ahead? yes or no: ').lower()
 if choice1=="no":
-   choice2= input("Its dark out here & you can see light coming from a house 500m away. would you like to explore? yes or no")
+   choice2= input('''\nIts dark & cold out here & you can see light coming from a house 500m away. would you like to take shelter? yes or no: ''').lower()
+   
+   if choice2=="yes":
+      choice3=input('''\nKnock! knock! an old lady opened the door with a knife smiling at you.\n"kill" or "Run"''').lower()
+      
+      if choice3=="kill":
+         print('''\nYou saw chopped onions & realised she was not going to attack\nYou decide to sleep.''')
+         
+         choice4=input('\ntring tring, its morning wanna wake up? "yes" or "no"?: ').lower()
+         if choice4=="yes":
+            print("\nIt was all a Dream")
+         else:
+            print("\nThe old lady's husband came back & saw his wife dead he killed you")
+
+      else:
+         print("\nDude go play subway surfer if you are this coward")
+   else:
+     print("\nIts a game of Guts, you died because you are not brave enough")
 else:
-    print("Whe there's a Board saying danger ahead only a fool will go ahead\nYou are dead because of stupidity.")
+    print("\nThere's a Board saying danger ahead, only a fool will go ahead\nYou are dead because of stupidity.")
